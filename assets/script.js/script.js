7137acb2c38fc6703eaa475b8cd7fa5cbbde1544
Levangul <150108077+Levangul@users.noreply.github.com>
2024-01-31 23:10:16 -0500

    url search by city and filtered types

--- assets/script.js/script.js
+++ assets/script.js/script.js
@@ -1,2 +1,34 @@
+//* added API ids and response for events
+const eventId = "Mzk2NjkwMTZ8MTcwNjU3NDExNS40OTM3MDc"
+const wikiId = "2798ba09d7fab878ba7c6d67fc1ac123"
+
+//'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={artist}&format=json'
+
+
+let city = "tbilisi"
+
+function displayEventsByCity() {
+  let eventCityUrl = 'https://api.seatgeek.com/2/events?venue.city=' + city + '&taxonomies.name=concert=&client_id=' + eventId
+  fetch(eventCityUrl)
+  .then(function(response) {
+    return response.json()
+  }).then(function(locationData) {
+   
+    console.log(locationData) 
+   } 
+    
+  ) 
+}
+
+  let button = document.querySelector('.search-button')
+  let searchedCity = document.querySelector('.city-name')
+    button.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    city = searchedCity.value;
+    displayEventsByCity(city)
+
+    
+});
 
 
