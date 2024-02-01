afd17e669319857851c1d78f9285653f8ac5aaa9
Levangul <150108077+Levangul@users.noreply.github.com>
2024-02-01 01:23:47 -0500

    corrections with actual data

--- assets/script.js/script.js
+++ assets/script.js/script.js
@@ -1,34 +1,88 @@
-//* added API ids and response for events
-const eventId = "Mzk2NjkwMTZ8MTcwNjU3NDExNS40OTM3MDc"
-const wikiId = "2798ba09d7fab878ba7c6d67fc1ac123"
-
-//'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={artist}&format=json'
 
 
-let city = "tbilisi"
+const wikiId = "2798ba09d7fab878ba7c6d67fc1ac123";
+const eventId = "Mzk2NjkwMTZ8MTcwNjU3NDExNS40OTM3MDc";
+let city = ""
 
-function displayEventsByCity() {
-  let eventCityUrl = 'https://api.seatgeek.com/2/events?venue.city=' + city + '&taxonomies.name=concert=&client_id=' + eventId
-  fetch(eventCityUrl)
-  .then(function(response) {
-    return response.json()
-  }).then(function(locationData) {
-   
-    console.log(locationData) 
-   } 
-    
-  ) 
+function searchEventsByCity() {
+    let eventCityUrl = 'https://api.seatgeek.com/2/events?venue.city=' + city + '&taxonomies.name=concert=&client_id=' + eventId
+    fetch(eventCityUrl)
+        .then(function (response) {
+            return response.json()
+        }).then(function (locationData) {
+            if (locationData.events.length === 0) {
+                console.error("No events found in " + city);
+                return;
+            }
+            
+        }
+
+        )
 }
 
-  let button = document.querySelector('.search-button')
-  let searchedCity = document.querySelector('.city-name')
-    button.addEventListener("click", function (event) {
+let button = document.getElementById('search-btn')
+let searchedCity = document.getElementById('search-input')
+button.addEventListener("click", function (event) {
     event.preventDefault();
 
     city = searchedCity.value;
-    displayEventsByCity(city)
+    searchEventsByCity()
+    displayEvents(city)
 
-    
+
 });
+
+const cards = document.querySelectorAll(".card");
+
+function displayEvents(city) {
+    const requestUrl = 'https://api.seatgeek.com/2/events?venue.city=' + city + '&taxonomies.name=concert=&client_id=' + eventId
+    // Fetch data from SeatGeek API
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+            // Update each card with performer details
+            cards.forEach((card, index) => {
+                const userName = document.createElement("h5");
+                const userUrl = document.createElement("p");
+
+                const flexColumn = card.querySelector(".flex-column");
+                flexColumn.innerHTML = "";
+
+                // Iterate through performers and create elements
+                data.events[index].performers.forEach((performer) => {
+                    const performerElement = document.createElement("h5");
+                    performerElement.textContent = performer.name;
+
+                    // Append performer element to the card
+                    flexColumn.appendChild(performerElement);
+                });
+
+                userName.textContent =
+                    "Performer: " + data.events[index].performers[0].name;
+                userUrl.textContent = "Event ID: " + data.events[index].id;
+
+                // Append user details to the card
+                flexColumn.append(userName);
+                flexColumn.append(userUrl);
+
+                // Update existing image element with performer image
+                const performerImageURL = data.events[index].performers[0].image;
+                if (performerImageURL) {
+                    const cardImage = card.querySelector("img");
+                    cardImage.src = performerImageURL;
+                    cardImage.alt = "Performer Image";
+                }
+            });
+        })
+        .catch(function (error) {
+            console.error("Error fetching data:", error);
+        });
+
+}
+
 
 
